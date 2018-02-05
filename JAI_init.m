bd192637280dca915253c4ddacce220cd6c818c1
Daniel Matthes <dmatthes@cbs.mpg.de>
2018-02-05 15:23:47 +0100

    Skript JAI_init – Skript aktualisiert, da der Ordner headmodels in layouts umbenannt wurde.

--- JAI_init.m
+++ JAI_init.m
@@ -1,4 +1,4 @@
 % -------------------------------------------------------------------------
 % Add directory and subfolders to path
 % -------------------------------------------------------------------------
-addpath('./:./easyplot:./elecorder:./functions:./general:./headmodels:./utilities');
+addpath('./:./easyplot:./elecorder:./functions:./general:./layouts:./utilities');
